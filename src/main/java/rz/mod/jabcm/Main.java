c016c23241cdcc8a22b5c23ce9722c0044e2f15a
RavenholmZombie <supersoftware1234@gmail.com>
2020-12-07 09:03:22 -0500

    Updated Branding

--- src/main/java/rz/mod/jabcm/Main.java
+++ src/main/java/rz/mod/jabcm/Main.java
@@ -17,7 +17,7 @@ public class Main
 {
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "jabcm";
-    public static final String VERSION = "0.0.5-1.16.3";
+    public static final String VERSION = "1.16.4-0.0.5";
 
     public Main()
     {
